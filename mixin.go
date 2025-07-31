130d11d267d04daf4060983ad9e5b1f8922df186
Raymond Liu <starainx@gmail.com>
2025-07-31 16:14:42 +0800

    fix readme errors.

--- mixin.go
+++ mixin.go
@@ -14,7 +14,7 @@ type TimeMixin struct {
 
 func (TimeMixin) Fields() []ent.Field {
 	return []ent.Field{
-		field.Time("created_at").Default(time.Now()).Comment("创建时间").Immutable(),
-		field.Time("updated_at").Default(time.Now()).Comment("更新时间").UpdateDefault(time.Now()),
+		field.Time("created_at").Default(time.Now).Comment("创建时间").Immutable(),
+		field.Time("updated_at").Default(time.Now).Comment("更新时间").UpdateDefault(time.Now),
 	}
 }
